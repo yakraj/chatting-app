5b91e91c73533a90a64901d5903fa7b434444e44
yakraj <princeyakraj289@gmail.com>
2023-09-05 10:15:01 +0530

    login

--- src/components/auth/login.page.js
+++ src/components/auth/login.page.js
@@ -1,31 +1,66 @@
-import React from "react";
-import "./style.css";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { MainContext } from "../../services/main.context";
+// import "./style.css";
+
 export const LoginPage = () => {
+  const { UserLogin } = useContext(MainContext);
+
+  const navigation = useNavigate();
+  const [umail, setumail] = useState("");
+  const [pass, setpass] = useState("");
+
+  const [error, setError] = useState("");
+
   return (
     <div className="form-container-box">
       <div className="form-container">
         <p className="title">Login</p>
-        <form className="form">
+        <form onSubmit={(e) => e.preventDefault(acn)} className="form">
           <div className="input-group">
             <label htmlFor="username">Username</label>
-            <input type="text" name="username" id="username" placeholder />
+            <input
+              onChange={(e) => {
+                setumail(e.target.value);
+              }}
+              type="text"
+              name="username"
+              id="username"
+              placeholder
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" placeholder />
+            <input
+              onChange={(e) => setpass(e.target.value)}
+              type="password"
+              name="password"
+              id="password"
+              placeholder
+            />
             <div className="forgot">
-              <a rel="noopener noreferrer" href="#">
-                Forgot Password ?
-              </a>
+              <a href="#">Forgot Password ?</a>
             </div>
           </div>
-          <button className="sign">Sign in</button>
+          <button
+            type="buttom"
+            onClick={() => {
+              if (umail.length > 3 && pass.length > 3) {
+                UserLogin(umail, pass);
+              } else {
+                setError("please fill required field");
+              }
+            }}
+            className="sign"
+          >
+            Sign in
+          </button>
           <div className="social-icons">
             <p className="signup-container">
               Don't have an account?
-              <a rel="noopener noreferrer" href="/signup" className>
+              <div onClick={() => navigation("/signup")} className="linktext">
                 Sign up
-              </a>
+              </div>
             </p>
           </div>
         </form>
